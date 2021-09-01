a94b173923bd131d03cc7b1fab820fa28a984e43
dpbam <derekphtpt@gmail.com>
2021-09-01 15:20:51 -0600

    a bunch of routes and other things 18.3.5

--- controllers/comment-controller.js
+++ controllers/comment-controller.js
@@ -1,4 +1,4 @@
-const { Comment, Pizza } = require('../models');
+const { Comment, Pizza, Reply } = require('../models');
 
 const commentController = {
     // add comment to pizza
@@ -19,6 +19,33 @@ const commentController = {
                 }
                 res.json(dbPizzaData);
             })
+            .catch(err => res.json(err));
+    },
+
+    addReply({ params, body }, res) {
+        console.log(body);
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $push: { replies: body } },
+            { new: true }
+        )
+            .then(dbPizzaData => {
+                if (!dbPizzaData) {
+                    res.status(404).json({ message: 'No pizza found with this id! '});
+                    return;
+                }
+                res.json(dbPizzaData);
+            })
+            .catch(err => res.json(err));
+    },
+    
+    removeReply({ params }, res) {
+        Comment.findOneAndUpdate(
+            { _id: params.commentId },
+            { $pull: { replies: { replyId: params.replyId } } },
+            { new: true }
+        )
+            .then(dbPizzaData => res.json(dbPizzaData))
             .catch(err => res.json(err));
     },
 
